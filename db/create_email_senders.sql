e908a21969c22e91afee32d7d50cd8786860868e
MuelitasVill <mmartinezve@unal.edu.co>
2026-04-23 23:34:35 -0500

    update: create email senders

--- db/create_email_senders.sql
+++ db/create_email_senders.sql
@@ -38,36 +38,36 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('alertas_med@unal.edu.co',                              NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_biblioteca@unal.edu.co',                       NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_comunicaciones@unal.edu.co',                   NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_direccion_administrativa@unal.edu.co',         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_direccion_laboratorios@unal.edu.co',           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_fac_ciencias_humanas_y_economicas@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_juridica@unal.edu.co',                         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('inf_aplicaciones_med@unal.edu.co',                     NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_vicerrectoria@unal.edu.co',                    NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_bienestar_universitario@unal.edu.co',          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('infservcomp_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('inflogistica_med@unal.edu.co',                         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_fac_ciencias@unal.edu.co',                     NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_fac_minas@unal.edu.co',                        NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_fac_ciencias_agrarias@unal.edu.co',            NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('info_aplica_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_secretaria_sede@unal.edu.co',                  NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('innovaacad_med@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('unalternativac_nal@unal.edu.co',                       NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('pcm@unal.edu.co',                                      NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('postmaster_med@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('infeducontinua@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_direccion_academica@unal.edu.co',              NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_direccion_de_investigacion_y_extension@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_direccion_ordenamiento_y_desarrollo_fisico@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_fac_arquitectura@unal.edu.co',                 NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_registro_y_matricula@unal.edu.co',             NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('informa_unimedios@unal.edu.co',                        NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('infpersonal_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20),
-('reestudia_med@unal.edu.co',                            NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN', 'OWNER', 20)
+('alertas_med@unal.edu.co',                              NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_biblioteca@unal.edu.co',                       NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_comunicaciones@unal.edu.co',                   NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_direccion_administrativa@unal.edu.co',         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_direccion_laboratorios@unal.edu.co',           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_fac_ciencias_humanas_y_economicas@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_juridica@unal.edu.co',                         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('inf_aplicaciones_med@unal.edu.co',                     NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_vicerrectoria@unal.edu.co',                    NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_bienestar_universitario@unal.edu.co',          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('infservcomp_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('inflogistica_med@unal.edu.co',                         NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_fac_ciencias@unal.edu.co',                     NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_fac_minas@unal.edu.co',                        NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_fac_ciencias_agrarias@unal.edu.co',            NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('info_aplica_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_secretaria_sede@unal.edu.co',                  NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('innovaacad_med@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('unalternativac_nal@unal.edu.co',                       NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('pcm@unal.edu.co',                                      NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('postmaster_med@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('infeducontinua@unal.edu.co',                           NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_direccion_academica@unal.edu.co',              NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_direccion_de_investigacion_y_extension@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_direccion_ordenamiento_y_desarrollo_fisico@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_fac_arquitectura@unal.edu.co',                 NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_registro_y_matricula@unal.edu.co',             NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('informa_unimedios@unal.edu.co',                        NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('infpersonal_med@unal.edu.co',                          NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20),
+('reestudia_med@unal.edu.co',                            NULL, 'HEADQUARTERS', 'SEDE MEDELLÍN ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -84,12 +84,12 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('ventanilla_man@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20),
-('bienestar_man@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20),
-('planea_man@unal.edu.co',       NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20),
-('postmaster_man@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20),
-('vicsede_man@unal.edu.co',      NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20),
-('estudiantilcs_man@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE MANIZALES', 'OWNER', 20)
+('ventanilla_man@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20),
+('bienestar_man@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20),
+('planea_man@unal.edu.co',       NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20),
+('postmaster_man@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20),
+('vicsede_man@unal.edu.co',      NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20),
+('estudiantilcs_man@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE MANIZALES ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -106,9 +106,9 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('unnoticias_pal@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE PALMIRA', 'OWNER', 20),
-('postmaster_pal@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE PALMIRA', 'OWNER', 20),
-('estudiantilcs_pal@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE PALMIRA', 'OWNER', 20)
+('unnoticias_pal@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE PALMIRA ESTUDIANTE', 'OWNER', 20),
+('postmaster_pal@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE PALMIRA ESTUDIANTE', 'OWNER', 20),
+('estudiantilcs_pal@unal.edu.co',NULL, 'HEADQUARTERS', 'SEDE PALMIRA ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -125,7 +125,7 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('divcultural_ori@unal.edu.co',  NULL, 'HEADQUARTERS', 'SEDE ORINOQUÍA', 'OWNER', 20)
+('divcultural_ori@unal.edu.co',  NULL, 'HEADQUARTERS', 'SEDE ORINOQUÍA ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -142,10 +142,10 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('secsedelapaz@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ', 'OWNER', 20),
-('sedelapaz@unal.edu.co',        NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ', 'OWNER', 20),
-('tics_paz@unal.edu.co',         NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ', 'OWNER', 20),
-('vicesedelapaz@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ', 'OWNER', 20)
+('secsedelapaz@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ ESTUDIANTE', 'OWNER', 20),
+('sedelapaz@unal.edu.co',        NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ ESTUDIANTE', 'OWNER', 20),
+('tics_paz@unal.edu.co',         NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ ESTUDIANTE', 'OWNER', 20),
+('vicesedelapaz@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE DE LA PAZ ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -162,13 +162,13 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, sede_code, role, priority)
 VALUES
-('divulgaciondrm_bog@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('reprecarrera_bog@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('comunicaciones_bog@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('diracasede_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('dircultural_bog@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('notificass_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20),
-('postmaster_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ', 'OWNER', 20)
+('divulgaciondrm_bog@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('reprecarrera_bog@unal.edu.co',   NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('comunicaciones_bog@unal.edu.co', NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('diracasede_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('dircultural_bog@unal.edu.co',    NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('notificass_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20),
+('postmaster_bog@unal.edu.co',     NULL, 'HEADQUARTERS', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 20)
 ON DUPLICATE KEY UPDATE
   name=COALESCE(VALUES(name), email_sender.name),
   org_type='HEADQUARTERS',
@@ -186,17 +186,17 @@ ON DUPLICATE KEY UPDATE
 -- =====================================
 INSERT INTO email_sender (email, name, org_type, org_code, sede_code, role, priority)
 VALUES
-('correo_fchbog@unal.edu.co', 'Facultad de Ciencias Humanas - Bogotá', 'SCHOOL', 'FACULTAD DE CIENCIAS HUMANAS', 'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_fibog@unal.edu.co',  'Facultad de Ingeniería - Bogotá',       'SCHOOL', 'FACULTAD DE INGENIERÍA',       'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_fcbog@unal.edu.co',  'Facultad de Ciencias - Bogotá',         'SCHOOL', 'FACULTAD DE CIENCIAS',          'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_farbog@unal.edu.co', 'Facultad de Artes - Bogotá',            'SCHOOL', 'FACULTAD DE ARTES',             'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_fcebog@unal.edu.co', 'Facultad de Ciencias Económicas - Bogotá','SCHOOL','FACULTAD DE CIENCIAS ECONÓMICAS','SEDE BOGOTÁ','OWNER',30),
-('correo_fmbog@unal.edu.co',  'Facultad de Medicina - Bogotá',         'SCHOOL', 'FACULTAD DE MEDICINA',          'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_fdbog@unal.edu.co',  'Facultad de Derecho, Ciencias Políticas y Sociales - Bogotá','SCHOOL','FACULTAD DE DERECHO, CIENCIAS POLÍTICAS Y SOCIALES','SEDE BOGOTÁ','OWNER',30),
-('correo_fmvbog@unal.edu.co', 'Facultad de Medicina Veterinaria y de Zootecnia - Bogotá','SCHOOL','FACULTAD DE MEDICINA VETERINARIA Y DE ZOOTECNIA','SEDE BOGOTÁ','OWNER',30),
-('correo_fcabog@unal.edu.co', 'Facultad de Ciencias Agrarias - Bogotá','SCHOOL', 'FACULTAD DE CIENCIAS AGRARIAS', 'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_febog@unal.edu.co',  'Facultad de Enfermería - Bogotá',       'SCHOOL', 'FACULTAD DE ENFERMERÍA',        'SEDE BOGOTÁ', 'OWNER', 30),
-('correo_fobog@unal.edu.co',  'Facultad de Odontología - Bogotá',      'SCHOOL', 'FACULTAD DE ODONTOLOGÍA',       'SEDE BOGOTÁ', 'OWNER', 30)
+('correo_fchbog@unal.edu.co', 'Facultad de Ciencias Humanas - Bogotá', 'SCHOOL', 'FACULTAD DE CIENCIAS HUMANAS ESTUDIANTE', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_fibog@unal.edu.co',  'Facultad de Ingeniería - Bogotá',       'SCHOOL', 'FACULTAD DE INGENIERÍA ESTUDIANTE',       'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_fcbog@unal.edu.co',  'Facultad de Ciencias - Bogotá',         'SCHOOL', 'FACULTAD DE CIENCIAS ESTUDIANTE',          'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_farbog@unal.edu.co', 'Facultad de Artes - Bogotá',            'SCHOOL', 'FACULTAD DE ARTES ESTUDIANTE',             'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_fcebog@unal.edu.co', 'Facultad de Ciencias Económicas - Bogotá','SCHOOL','FACULTAD DE CIENCIAS ECONÓMICAS ESTUDIANTE','SEDE BOGOTÁ ESTUDIANTE','OWNER',30),
+('correo_fmbog@unal.edu.co',  'Facultad de Medicina - Bogotá',         'SCHOOL', 'FACULTAD DE MEDICINA ESTUDIANTE',          'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_fdbog@unal.edu.co',  'Facultad de Derecho, Ciencias Políticas y Sociales - Bogotá','SCHOOL','FACULTAD DE DERECHO, CIENCIAS POLÍTICAS Y SOCIALES','SEDE BOGOTÁ ESTUDIANTE','OWNER',30),
+('correo_fmvbog@unal.edu.co', 'Facultad de Medicina Veterinaria y de Zootecnia - Bogotá','SCHOOL','FACULTAD DE MEDICINA VETERINARIA Y DE ZOOTECNIA','SEDE BOGOTÁ ESTUDIANTE','OWNER',30),
+('correo_fcabog@unal.edu.co', 'Facultad de Ciencias Agrarias - Bogotá','SCHOOL', 'FACULTAD DE CIENCIAS AGRARIAS ESTUDIANTE', 'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_febog@unal.edu.co',  'Facultad de Enfermería - Bogotá',       'SCHOOL', 'FACULTAD DE ENFERMERÍA ESTUDIANTE',        'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30),
+('correo_fobog@unal.edu.co',  'Facultad de Odontología - Bogotá',      'SCHOOL', 'FACULTAD DE ODONTOLOGÍA ESTUDIANTE',       'SEDE BOGOTÁ ESTUDIANTE', 'OWNER', 30)
 ON DUPLICATE KEY UPDATE
   name=VALUES(name),
   org_type='SCHOOL',
