f134306bfc08d45bfc20b272b24ce7455f7bf616
MuelitasVill <mmartinezve@unal.edu.co>
2026-06-08 13:58:19 -0500

    tiny fix: change access token expire

--- app/service/use_cases/auth.py
+++ app/service/use_cases/auth.py
@@ -16,7 +16,7 @@ from app.exceptions.auth_exceptions import (
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 120
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
